2ae4b68b9a684fa7022bea1a10dc9370b107967b
Mohammadreza jsh <m.jahanshahi021@gmail.com>
2023-08-03 15:10:11 +0330

    Initial Commit

---
+++ Login SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Login SwiftUI
+//
+//  Created by mohammadreza on 5/12/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login SwiftUI/Login_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_SwiftUIApp.swift
+//  Login SwiftUI
+//
+//  Created by mohammadreza on 5/12/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct Login_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
